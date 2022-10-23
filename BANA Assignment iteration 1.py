73bebd516562213086314add312dbd45b9da0f8c
Aaron Liu <aar474@gmail.com>
2022-10-22 20:19:33 -0400

    added the part for the top 4 most leading causes of deaths

--- BANA Assignment iteration 1.py
+++ BANA Assignment iteration 1.py
@@ -89,3 +89,67 @@ cleanDF = pd.DataFrame(df2.iloc[4:-5])
 cleanDF.columns = newColumnHeaders
 ## Get rid of row number 60 as it was just an empty row
 cleanDF.dropna()
+
+
+# ### Get the four leading causes of Deaths
+
+## Should it be by year? should we just aggregate all deaths specific to a cause name?
+
+# ### Get the Years column sorted
+df1_sorted_by_year = df1.sort_values(by=['Year'])
+
+df1_sorted_by_year # 10296 rows
+
+# ### Get all the unique causes: 11 Unique Causes (936 each)
+df1_sorted_by_year.iloc[0:, 1].value_counts()
+"""
+All Causes                                                               936
+Alzheimer's disease (G30)                                                936
+Chronic lower respiratory diseases (J40-J47)                             936
+Nephritis, nephrotic syndrome and nephrosis (N00-N07,N17-N19,N25-N27)    936
+Malignant neoplasms (C00-C97)                                            936
+Accidents (unintentional injuries) (V01-X59,Y85-Y86)                     936
+Diabetes mellitus (E10-E14)                                              936
+Cerebrovascular diseases (I60-I69)                                       936
+Influenza and pneumonia (J09-J18)                                        936
+Intentional self-harm (suicide) (*U03,X60-X84,Y87.0)                     936
+Diseases of heart (I00-I09,I11,I13,I20-I51)                              936
+Name: 113 Cause Name, dtype: int64
+"""
+
+## Display only these three columns 
+df1_causes_and_death = df1_sorted_by_year[['113 Cause Name', 'Cause Name', 'Deaths']]
+# df1_causes_and_death
+
+#####################################################################
+### The Below was used to help understand one type of cause of death
+
+## Find the unique count of the causes
+# df1_causes_and_death.iloc[0:, 1].value_counts()
+
+
+## Get the deaths for a specific cause name
+# clrd_deaths = df1_causes_and_death[df1_causes_and_death.iloc[0:,1].isin(['CLRD'])]
+# clrd_deaths
+
+## Get the cumulative count: 4869452 Deaths by CLRD
+# clrd_deaths.iloc[0:,2].agg('sum')
+#####################################################################
+
+## Use pivot table to help aggregate all the causes and summing their deaths
+total_deaths_per_causes = pd.pivot_table(df1_causes_and_death,
+                                         values='Deaths',
+                                         index=['Cause Name'],
+                                         #columns=['113 Cause Name'],
+                                         aggfunc=np.sum)
+
+#total_deaths_per_causes
+
+## Sort the pivot table in descending order
+deaths_descending = total_deaths_per_causes.sort_values(by=['Deaths'], ascending=False)
+# deaths_descending
+
+## Display the top 4
+top_4_deaths = deaths_descending.iloc[0:4]
+top_4_deaths
+
